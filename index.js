4fb1062f2245a46b7c2b3acee40e8936c74aff16
Michael Oldacre <mloldacre@live.com>
2020-07-19 02:05:30 -0400

    step 3 done

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 'use strict';
 
 function getDogImage() {
-  let num = $('input[type=number]').val();
-  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+  //let num = $('input[type=number]').val();
+  let hound = $('#dog-breed').val();
+  console.log(hound);
+
+  fetch(`https://dog.ceo/api/breed/${hound}/images/random`)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson.message))
+    .then(responseJson =>
+      displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  responseJson.forEach(image => {$('.results').append(
+
+  //display array of images
+  /* responseJson.forEach(image => {$('.results').append(
     `<img src=${image} class="results-img" alt="placeholder">`);
-  });
-  
-  /* $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`); */
-  //display the results section
-  $('.results').removeClass('hidden');
+  }); */
+
+  if (responseJson.status === 'error') {
+    alert(`${responseJson.message}`);
+  }
+  else{ //replace the existing image with the new one
+    $('.results-img').replaceWith(
+      `<img src="${responseJson.message}" class="results-img">`);
+    //display the results section
+    $('.results').removeClass('hidden');}
+
 }
 
 function watchForm() {
   $('form').submit(event => {
-    $('.results').empty();
+    //$('.results').empty();
     event.preventDefault();
     getDogImage();
   });
 }
 
-$(function() {
+$(function () {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
